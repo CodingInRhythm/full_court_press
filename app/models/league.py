f219a4aa42ffa168a5bf450f85d49a1bcc3a875f
Alex Clough <alexcl123@gmail.com>
2021-06-02 20:28:19 -0400

    added code for players attribute on League model

--- app/models/league.py
+++ app/models/league.py
@@ -1,6 +1,7 @@
 from .db import db
 from .user import User
 from .leaguemembers import league_members
+from .player_cards import player_cards
 
 
 
@@ -14,12 +15,20 @@ class League(db.Model):
     users_in = db.relationship("User",
     secondary=league_members, back_populates="leagues_in")
     
-    cards_in = db.relationship("")
+    # code below:
+    # players is a collection of objects of type Player using the
+    #player_cards join table to find only the players who are matched up
+    # with League's instance id
+
+    players = db.relationship("Player", secondary=player_cards)
+
+    teams = db.relationship("Team", secondary=player_cards)
 
     def to_dict(self):
         return {
         "id": self.id,
         "name": self.name
+        # [player.to_dict for player in self.players]
         }
 
     
