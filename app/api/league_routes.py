c03fb56ef930bda2aa8f76ef7e0a7f0cfdc51ee7
Alex Clough <alexcl123@gmail.com>
2021-06-03 10:25:38 -0400

    leagues slash route working

--- app/api/league_routes.py
+++ app/api/league_routes.py
@@ -3,8 +3,14 @@ from app.models import User, db
 from app.forms import LoginForm
 from app.forms import SignUpForm
 from flask_login import current_user, login_user, logout_user, login_required
+from ..models.league import League
 
 league_routes = Blueprint('leagues', __name__)
+
+@league_routes.route('/')
+def leagues():
+    leagues = League.query.all()
+    return {"leagues": [league.to_dict() for league in leagues]}
 
 @league_routes.route('/<int:id>')
 def players():
