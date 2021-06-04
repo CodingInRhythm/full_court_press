041585eb8838a41f3033d7cbf801896837537d2b
Alex Clough <alexcl123@gmail.com>
2021-06-03 21:19:00 -0400

    can use redux store to list out users leagues in sidebar

--- react-app/src/components/Sidebar/index.js
+++ react-app/src/components/Sidebar/index.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import {useSelector} from 'react-redux'
 import "./Sidebar.css"
 const Sidebar = () => {
+
+
+    const leagues = useSelector((state) => state.league)
+    console.log(leagues)
     return (
         <div className="sidebar-container">
-            THERE
+            {Object.keys(leagues).length > 0 ?  (
+            <ul>
+                {Object.keys(leagues).map((league) => {
+                   return (
+                       <li>
+                           {leagues[league].name}
+                       </li>
+                   )
+                })}
+            </ul>
+
+            ) : (
+                <div>Join a league!</div>
+            )}
         </div>
     )
 }
