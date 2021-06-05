ce79fcd49542ba9d87e7d46bf76d4ea0c6c80405
Alex Clough <alexcl123@gmail.com>
2021-06-04 20:00:53 -0400

    working thru sqlalchemy bug in team_routes

--- app/api/team_routes.py
+++ app/api/team_routes.py
@@ -1,9 +1,8 @@
 from flask import Blueprint, request
-from app.models import User, db
+from app.models import User, db, Player, Team
 from app.forms import LoginForm
 from app.forms import SignUpForm
 from flask_login import current_user, login_user, logout_user, login_required
-from ..models.team import Team
 
 team_routes = Blueprint('teams', __name__)
 
@@ -12,9 +11,12 @@ def teams():
     teams = Team.query.all()
     return {"teams": [team.to_dict() for team in teams]}
 
-@team_routes.route('/<int:id>')
-def players():
-    pass
+@team_routes.route('/<int:teamid>', methods=['DELETE'])
+def remove_players(teamid):
+    team = Team.query.filter(Team.id == teamid)
+    player = Player.query.filter(Player.id == 1)
+    team.players.remove(player)
+    return "something"
 
 @team_routes.route('/add', methods=['POST'])
 def add_team():
--- react-app/src/components/TeamPage/index.js
+++ react-app/src/components/TeamPage/index.js
@@ -2,44 +2,61 @@ import React, {useEffect, useState} from 'react'
 import {useSelector, useDispatch} from 'react-redux'
 import {useLocation} from 'react-router-dom'
 
+import {setCurrentTeam} from "../../store/team"
+
 const TeamPage = () => {
     const team = useSelector((state) => state.league)
     const dispatch = useDispatch()
 
     const [players, setPlayers] = useState([])
+    const [currentTeamState, setCurrentTeamState] = useState({})
     let location = useLocation()
     let path = location.pathname.split("/");
     let teamid = Number(path[path.length - 1]);
     let leagueteams;
-    let currenteam;
+    
    
 
     if (team.currentleague && players.length < 1) {
         console.log(team.currentleague.players)
         setPlayers(team.currentleague.players)
         leagueteams = team.currentleague.teams
-        for (i=0; i < leagueteams.length; i++) {
+        for (let i=0; i < leagueteams.length; i++) {
+            console.log(leagueteams[i])
+            console.log(leagueteams[i].id)
             if (leagueteams[i].id === teamid) {
-                currentteam = leagueteams[i]
+                console.log('SHOUKD MAKE IT')
+                setCurrentTeamState(leagueteams[i])
             }
         }
+       
     }
-    /*Strategy here:
-    I want current team to my redux store and basically dispatch a setCurrentTeam here on the first render.  
-    It'll have all the info it needs to 
-    */
+
+    const dropPlayer = (id) => {
+        fetch(`/api/teams/${currentTeamState.id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(id)
+        })
+    }
 
     useEffect(() => {
-        // dispatch()
+        dispatch(setCurrentTeam(currentTeamState))
         console.log(players)
 
     }, [])
     return (
         <>
             <h1>ANOTHER BATTLE OVERCOME</h1>
-            {players.length > 0 && players.map((player) => {
-                return <h1 key={player.id}>{player.name}</h1>
-            })}
+            {currentTeamState.players?.length > 0 && currentTeamState.players.map((player) => {
+                return (
+                    <>
+                        <h1>{player.name}</h1>)
+                        <button onClick={() => dropPlayer(player.id)}>Drop Player</button>
+                    </>
+            )})}
         </>
         
     )
